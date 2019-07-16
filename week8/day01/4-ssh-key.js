1da51d74fddf6ba3b1f5d28a370c5180a4e49eb4
caicai1234567890 <1006994160@qq.com>
2019-07-16 21:43:25 +0800

    git 的引用

--- week8/day01/4-ssh-key.js
+++ week8/day01/4-ssh-key.js
@@ -5,4 +5,10 @@
 // 使用ssh-key的步骤:
 // 1.生成 ssh-key 
 // 1.1 进入到家目录中,在git bash中输入 ~
-// 1.2 进入到家目录.ssh中 cd .ssh 
+// 1.2 进入到家目录.ssh中 cd .ssh ,如果没有可以创建: mkdir .ssh
+// 3. 执行 ssh-keygen 一路回车即可
+// 4. cat id_rsa.pub 文件
+// 5. 复制上一步cat出来的结果
+// 6.打开github或者gitlab,进入个人设置 settings -> SSH and GPG keys -> new SSHkey
+// 7. title是对这个key的描述,把复制的key复制到key中,点击 Add SSH key .
+// 8. 后面再clone项目时改用SSH协议,以后的操作都不需要密码
